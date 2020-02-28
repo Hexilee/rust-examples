018ebebff411a08adda3648fed7f089ef3bfe4e3
Hexilee <hexileee@gmail.com>
2020-02-28 21:14:24 +0800

    add tls

---
+++ tls/src/main.rs
@@ -0,0 +1,21 @@
+use hyper::client::{Client, HttpConnector};
+use hyper::Body;
+use hyper_tls::native_tls;
+use hyper_tls::HttpsConnector;
+use tokio_tls::TlsConnector;
+use http::StatusCode;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let native_tls_connector = native_tls::TlsConnector::new()?;
+    let tls_connector = TlsConnector::from(native_tls_connector);
+    let http_connector = HttpConnector::new();
+    let mut https_connector = HttpsConnector::from((http_connector, tls_connector));
+    https_connector.https_only(true);
+    let client = Client::builder().build::<_, Body>(https_connector);
+    let resp = client
+        .get("https://github.com".parse()?)
+        .await?;
+    assert_eq!(StatusCode::OK, resp.status());
+    Ok(())
+}
